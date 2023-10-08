89e2a79c80811cf2cd6ae51642f258c4f2343d66
kirikq <144107450+kirikq@users.noreply.github.com>
2023-10-08 22:58:39 +0300

    Add files via upload

---
+++ Barutkin_lr3_4__8.pas
@@ -0,0 +1,13 @@
+﻿program lr83_4;
+var l,i:int64;
+sum:real;
+begin
+  write('Введите l ');
+  readln(l);
+  sum:=0;
+  for i:= 1 to l do
+  begin
+    sum:=sum+1/i;
+  end;
+  writeln('Сумма равна ', sum);
+end.
---
+++ Barutkin_lr3_4__9.pas
@@ -0,0 +1,13 @@
+﻿program lr93_4;
+var l,i:int64;
+p:real;
+begin
+  write('Введите l ');
+  readln(l);
+  p:=1;
+  for i:= 1 to l do
+  begin
+    p:=p*(1/i);
+  end;
+  writeln('Произведение равно ', p);
+end.
